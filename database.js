d5aa8fe4f070e598a52ca8e4e037849d7a115b5d
Alexandre <als.stumpf@gmail.com>
2026-04-14 18:26:09 -0300

    criando arquivos do banco de dados

--- database.js
+++ database.js
@@ -2,5 +2,8 @@ const sqlite3 = require('sqlite3');
 const {open} = require('sqlite');
 
 const criarBanco = async() => {
-    
+    const db = await open({
+        filename: '/database.db',
+        driver: sqlite3.Database
+    });
 };
