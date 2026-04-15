b39e1c43a8bc7834f13d557b630d237559d64075
Alexandre <als.stumpf@gmail.com>
2026-04-15 18:57:59 -0300

    criando a porta para rodar o servidor localmente

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const {criarBanco} = require('./database');
 const app = express();
 
 app.use(express.json());
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
